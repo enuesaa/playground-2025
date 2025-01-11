b66879bde82bc39bf5ddf91ff897c22c35ccf019
nua <enuesaa@gmail.com>
2025-01-12 01:03:36 +0900

    feat(laravel-graphql): try lighthouse as graphql lib

--- .devdev/laravel-graphql-app/README.md
+++ .devdev/laravel-graphql-app/README.md
@@ -13,3 +13,10 @@
 
 ## Links
 - https://qiita.com/fujita-goq/items/8942d76dd2f542cb3ae0
+
+## Lighthouse
+- Laravel の GraphQL ライブラリ
+- Provider を読み込むと /graphql にエンドポイントが生える。また /graphiql で devtool が立ち上がる
+- スキーマを定義したら PHP を書く必要なく、そのまま DB を見に行く模様
+- HTTP レイヤーが DB に密結合している感じが Laravel らしいなと思った。
+- resolver を PHP で書きたいので、調査中
--- .devdev/laravel-graphql-app/bootstrap/app.php
+++ .devdev/laravel-graphql-app/bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// なんかいるらしいけど laravel 11 でどう記述すればいいか分からない
+// $app->configure('lighthouse');
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
