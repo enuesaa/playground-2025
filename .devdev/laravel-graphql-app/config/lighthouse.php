36b2cd2afcb28bef26f2023c4dd06622c9f21ac8
nua <enuesaa@gmail.com>
2025-01-31 23:35:59 +0900

    chore(laravel-graphql): on pint, use laravel preset. 設定ファイルを自前でメンテナンスしたくないので

--- .devdev/laravel-graphql-app/config/lighthouse.php
+++ .devdev/laravel-graphql-app/config/lighthouse.php
@@ -440,17 +440,17 @@ return [
             'echo' => [
                 'driver' => 'echo',
                 'connection' => env('LIGHTHOUSE_SUBSCRIPTION_REDIS_CONNECTION', 'default'),
-                'routes' => Nuwave\Lighthouse\Subscriptions\SubscriptionRouter::class . '@echoRoutes',
+                'routes' => Nuwave\Lighthouse\Subscriptions\SubscriptionRouter::class.'@echoRoutes',
             ],
             'pusher' => [
                 'driver' => 'pusher',
                 'connection' => 'pusher',
-                'routes' => Nuwave\Lighthouse\Subscriptions\SubscriptionRouter::class . '@pusher',
+                'routes' => Nuwave\Lighthouse\Subscriptions\SubscriptionRouter::class.'@pusher',
             ],
             'reverb' => [
                 'driver' => 'pusher',
                 'connection' => 'reverb',
-                'routes' => Nuwave\Lighthouse\Subscriptions\SubscriptionRouter::class . '@reverb',
+                'routes' => Nuwave\Lighthouse\Subscriptions\SubscriptionRouter::class.'@reverb',
             ],
         ],
 
