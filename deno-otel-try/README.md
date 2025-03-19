455d31853a69594d1b61ea411bbc5032b45d6b6f
nua <enuesaa@gmail.com>
2025-03-20 00:19:49 +0900

    feat: add comments to deno otel

--- deno-otel-try/README.md
+++ deno-otel-try/README.md
@@ -2,6 +2,11 @@
 
 - deno v2.2 でトレースデータを送れるようになった模様
 - まだ unsafe
+- まあ Deno.serve があるし、対応しても不思議ではないかな
+- otel collector に送る処理が隠蔽されているのは不思議な感覚
+  - Go とかでもライブラリとかが隠蔽するが、それはそれとしてフックがある
+  - trace provider とか何も記述せずに単に送れるのはちょっと不思議
 
 ## Links
+
 - https://docs.deno.com/runtime/fundamentals/open_telemetry/
--- deno-otel-try/main.ts
+++ deno-otel-try/main.ts
@@ -1,21 +1,37 @@
-import { trace } from "npm:@opentelemetry/api@1";
+import { SpanStatusCode, trace } from "npm:@opentelemetry/api@1";
 
 const INDEX_ROUTE = new URLPattern({ pathname: "/" });
 const BOOK_ROUTE = new URLPattern({ pathname: "/book/:id" });
 
 Deno.serve(async (req) => {
   const span = trace.getActiveSpan();
+
   if (INDEX_ROUTE.test(req.url)) {
-    span.setAttribute("http.route", "/");
-    span.updateName(`${req.method} /`);
+    span?.setAttribute("http.route", "/");
+    span?.updateName(`${req.method} /`);
 
-    // handle index route
-  } else if (BOOK_ROUTE.test(req.url)) {
-    span.setAttribute("http.route", "/book/:id");
-    span.updateName(`${req.method} /book/:id`);
+    const res = await fetch("https://example.com");
+    console.log(res.status);
 
-    // handle book route
+    span?.setStatus({
+      code: SpanStatusCode.OK,
+      message: "ok",
+    });
+    span?.setAttribute("a", "aaa");
+
+    return new Response("ok", { status: 200 });
+  }
+  if (BOOK_ROUTE.test(req.url)) {
+    span?.setAttribute("http.route", "/book/:id");
+    span?.updateName(`${req.method} /book/:id`);
+
+    span?.setStatus({
+      code: SpanStatusCode.ERROR,
+      message: "err",
+    });
+
+    return new Response("ok", { status: 400 });
   }
 
-  return new Response("Not found", { status: 404 })
+  return new Response("Not found", { status: 404 });
 });
