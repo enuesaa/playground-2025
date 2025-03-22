753f525eca5a2fa1c01404c6110c8ec385df56ea
nua <enuesaa@gmail.com>
2025-03-22 14:17:49 +0900

    chore(laravel): add memo

--- laravel-graphql-try/app/Http/Controllers/ProtoController.php
+++ laravel-graphql-try/app/Http/Controllers/ProtoController.php
@@ -11,6 +11,11 @@ class ProtoController extends Controller
     public function index()
     {
         $apikey = '';
+
+        // Asteroids - NeoWs を見ようかな
+        // https://api.nasa.gov/neo/rest/v1/feed?api_key=DEMO_KEY
+
+        // たぶん日単位でデータが変わるので、面白そう？
     
         $res = Http::get("https://api.nasa.gov/planetary/apod?api_key={$apikey}");
 
