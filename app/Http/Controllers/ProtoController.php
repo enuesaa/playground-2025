0ada59eb3f0adbcb29ab36198fa08ad3874f51a5
nua <enuesaa@gmail.com>
2025-03-23 19:36:51 +0900

    feat: use Guzzele to fetch nasa api

--- app/Http/Controllers/ProtoController.php
+++ app/Http/Controllers/ProtoController.php
@@ -3,23 +3,21 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
 
 class ProtoController extends Controller
 {
     public function index()
-    {
-        $apikey = '';
-
+    {        
         // Asteroids - NeoWs を見ようかな
         // https://api.nasa.gov/neo/rest/v1/feed?api_key=DEMO_KEY
-
         // たぶん日単位でデータが変わるので、面白そう？
-    
-        $res = Http::get("https://api.nasa.gov/planetary/apod?api_key={$apikey}");
 
-        $resbody = $res->json();
+        $apikey = config('aero.nasa.apikey');
+
+        $client = new Client();
+        $res = $client->get("https://api.nasa.gov/planetary/apod?api_key={$apikey}");
+        $resbody = json_decode($res->getBody()->getContents(), associative: true);
         var_dump($resbody);
     }
 }
