5d9ccf3657ad3a035554ffce3d0d3f00e1c4ba88
nua <enuesaa@gmail.com>
2025-05-24 15:29:40 +0900

    chore: 全体的に設定を見直し

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -14,5 +15,6 @@ final class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Vite::prefetch(concurrency: 3);
     }
 }
