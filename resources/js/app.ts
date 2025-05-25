a02218c33dc17e181e8060789fbe9181c16eca4a
nua <enuesaa@gmail.com>
2025-05-25 17:41:59 +0900

    chore: use `biome`

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,7 +4,4 @@ import App from './components/App.vue'
 import '../css/app.css'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
-createApp(App)
-  .use(router)
-  .use(VueQueryPlugin)
-  .mount('#app')
+createApp(App).use(router).use(VueQueryPlugin).mount('#app')
