0a51f7e64ef2e47d410118f779eaf743297f69c9
nua <enuesaa@gmail.com>
2025-05-31 18:55:18 +0900

    feat: use primevue

--- resources/ui/app.ts
+++ resources/ui/app.ts
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import { router } from './router'
 import App from './App.vue'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import PrimeVue from 'primevue/config'
 import './css/app.css'
 
-createApp(App).use(router).use(VueQueryPlugin).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(VueQueryPlugin)
+app.use(PrimeVue, {
+  unstyled: true,
+})
+
+app.mount('#app')
