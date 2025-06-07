a5f3bce3eefed9b30bbd01e6eaf80824f2145ac8
nua <enuesaa@gmail.com>
2025-06-07 14:51:22 +0900

    feat(openapi): use zircote/swagger-php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(title: 'My First API', version: '0.1')]
 abstract class Controller
 {
 }
--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\PictureResource;
 use App\Services\Nasa\Nasa;
 use App\UseCases\AstronomyPictureUseCase;
 use Illuminate\Contracts\Support\Responsable;
+use OpenApi\Attributes as OA;
 
 final class PictureController extends Controller
 {
@@ -16,6 +17,11 @@ final class PictureController extends Controller
     ) {
     }
 
+    #[OA\Get(path: '/picture', operationId: 'getPicture')]
+    #[OA\Response(
+        response: '200',
+        description: 'The data',
+    )]
     public function view(): Responsable
     {
         $usecase = new AstronomyPictureUseCase($this->nasa);
