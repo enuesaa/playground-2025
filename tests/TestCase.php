0ae1c3b520eecd78a702897cf4c96074c18cfcaf
nua <enuesaa@gmail.com>
2025-06-15 16:11:38 +0900

    feat(testing): 明らかにオーバーだが test contaienrs を使う

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,9 @@ namespace Tests;
 
 use Database\Seeders\TestingSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+use Testcontainers\Modules\MySQLContainer;
+use Testcontainers\Container\StartedGenericContainer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
@@ -13,6 +16,31 @@ abstract class TestCase extends BaseTestCase
     use RefreshDatabase;
 
     protected string $seeder = TestingSeeder::class;
+    protected StartedGenericContainer $mysql;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mysql = (new MySQLContainer('8.0'))
+            ->withMySQLDatabase('test')
+            ->withMySQLUser('test', 'test')
+            ->start();
+        config([
+            'database.connections.mysql.host' => 'host.docker.internal',
+            'database.connections.mysql.port' => $this->mysql->getMappedPort(3306),
+            'database.connections.mysql.database' => 'test',
+            'database.connections.mysql.username' => 'test',
+            'database.connections.mysql.password' => 'test',
+        ]);
+        Artisan::call('migrate');
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->mysql->stop();
+    }
 
     /**
      * @return TestResponse
