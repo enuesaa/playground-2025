55782ad1c9f16b7f630a75fc6b9350ca629a1834
nua <enuesaa@gmail.com>
2025-06-22 16:05:12 +0900

    chore(testing): test normally

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,28 +7,21 @@ namespace Tests;
 use Database\Seeders\TestingSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Tests\Traits\TestContainer;
 
 abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
-    use TestContainer;
 
     protected string $seeder = TestingSeeder::class;
 
     protected function setUp(): void
     {
-        $this->refreshApplication();
-        $this->setUpTestContainer();
-
         parent::setUp();
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
-
-        $this->tearDownTestContainer();
     }
 
     /**
