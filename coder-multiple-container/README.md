06093bc2d66db18f72c24804c0643dd73214c8a7
nua <enuesaa@gmail.com>
2025-07-05 00:46:27 +0900

    feat(coder): いったん立ち上がるようにした

---
+++ coder-multiple-container/README.md
@@ -0,0 +1,5 @@
+# coder
+
+## Commands
+```bash
+./coder server --postgres-url "postgres://coder:coderpass@localhost:5432/coder?sslmode=disable" --access-url "http://127.0.0.1:3000"
---
+++ coder-multiple-container/template/main.tf
@@ -0,0 +1,151 @@
+terraform {
+  required_providers {
+    coder = {
+      source = "coder/coder"
+    }
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+data "coder_parameter" "git_repo_url" {
+  name         = "git_repo_url"
+  display_name = "Git repository URL"
+  description  = "URL of the git repository to clone"
+}
+
+provider "docker" {}
+
+data "coder_provisioner" "me" {}
+data "coder_workspace" "me" {}
+data "coder_workspace_owner" "me" {}
+
+resource "coder_agent" "main" {
+  arch           = data.coder_provisioner.me.arch
+  os             = "linux"
+  startup_script = <<-EOT
+    set -e
+
+    if [ ! -f ~/.init_done ]; then
+      cp -rT /etc/skel ~
+      touch ~/.init_done
+    fi
+
+    echo a;
+    dockerd > /home/coder/dockerd.log 2>&1 &
+
+    # until docker info > /dev/null 2>&1; do
+    #   echo "Waiting for Docker to be ready..."
+    #   sleep 2
+    # done
+
+    # docker compose up -d
+
+    # mkdir -p /home/coder/project
+    # git clone $GIT_REPO_URL /home/coder/project
+    # chown -R coder:coder /home/coder/project
+  EOT
+
+  env = {
+    GIT_REPO_URL = data.coder_parameter.git_repo_url.value
+  }
+
+  metadata {
+    display_name = "CPU Usage"
+    key          = "0_cpu_usage"
+    script       = "coder stat cpu"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "RAM Usage"
+    key          = "1_ram_usage"
+    script       = "coder stat mem"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "Home Disk"
+    key          = "3_home_disk"
+    script       = "coder stat disk --path $${HOME}"
+    interval     = 60
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "CPU Usage (Host)"
+    key          = "4_cpu_usage_host"
+    script       = "coder stat cpu --host"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "Memory Usage (Host)"
+    key          = "5_mem_usage_host"
+    script       = "coder stat mem --host"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "Load Average (Host)"
+    key          = "6_load_host"
+    script   = <<EOT
+      echo "`cat /proc/loadavg | awk '{ print $1 }'` `nproc`" | awk '{ printf "%0.2f", $1/$2 }'
+    EOT
+    interval = 60
+    timeout  = 1
+  }
+
+  metadata {
+    display_name = "Swap Usage (Host)"
+    key          = "7_swap_host"
+    script       = <<EOT
+      free -b | awk '/^Swap/ { printf("%.1f/%.1f", $3/1024.0/1024.0/1024.0, $2/1024.0/1024.0/1024.0) }'
+    EOT
+    interval     = 10
+    timeout      = 1
+  }
+}
+
+module "code-server" {
+  count  = data.coder_workspace.me.start_count
+  source = "registry.coder.com/coder/code-server/coder"
+  version = "~> 1.0"
+  agent_id = coder_agent.main.id
+  order    = 1
+}
+
+resource "docker_container" "workspace" {
+  count = data.coder_workspace.me.start_count
+  name = "coder-${data.coder_workspace_owner.me.name}-${lower(data.coder_workspace.me.name)}"
+  image = "codercom/enterprise-base:ubuntu"
+  hostname = data.coder_workspace.me.name
+  entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
+  env     = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
+  # runtime = "sysbox-runc"
+  privileged = true
+
+  host {
+    host = "host.docker.internal"
+    ip   = "host-gateway"
+  }
+
+  volumes {
+    container_path = "/home/coder"
+    volume_name    = docker_volume.home_volume.name
+    read_only      = false
+  }
+}
+
+resource "docker_volume" "home_volume" {
+  name = "coder-${data.coder_workspace.me.id}-home"
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
