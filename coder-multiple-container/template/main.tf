66b917e1793730ded661568ea8b25cd5ee9167aa
nua <enuesaa@gmail.com>
2025-07-05 01:27:45 +0900

    feat: mac で dind できるようにした

--- coder-multiple-container/template/main.tf
+++ coder-multiple-container/template/main.tf
@@ -32,19 +32,10 @@ resource "coder_agent" "main" {
       touch ~/.init_done
     fi
 
-    echo a;
     dockerd > /home/coder/dockerd.log 2>&1 &
 
-    # until docker info > /dev/null 2>&1; do
-    #   echo "Waiting for Docker to be ready..."
-    #   sleep 2
-    # done
-
-    # docker compose up -d
-
-    # mkdir -p /home/coder/project
-    # git clone $GIT_REPO_URL /home/coder/project
-    # chown -R coder:coder /home/coder/project
+    mkdir -p /home/coder/app
+    git clone $GIT_REPO_URL /home/coder/app
   EOT
 
   env = {
@@ -74,42 +65,6 @@ resource "coder_agent" "main" {
     interval     = 60
     timeout      = 1
   }
-
-  metadata {
-    display_name = "CPU Usage (Host)"
-    key          = "4_cpu_usage_host"
-    script       = "coder stat cpu --host"
-    interval     = 10
-    timeout      = 1
-  }
-
-  metadata {
-    display_name = "Memory Usage (Host)"
-    key          = "5_mem_usage_host"
-    script       = "coder stat mem --host"
-    interval     = 10
-    timeout      = 1
-  }
-
-  metadata {
-    display_name = "Load Average (Host)"
-    key          = "6_load_host"
-    script   = <<EOT
-      echo "`cat /proc/loadavg | awk '{ print $1 }'` `nproc`" | awk '{ printf "%0.2f", $1/$2 }'
-    EOT
-    interval = 60
-    timeout  = 1
-  }
-
-  metadata {
-    display_name = "Swap Usage (Host)"
-    key          = "7_swap_host"
-    script       = <<EOT
-      free -b | awk '/^Swap/ { printf("%.1f/%.1f", $3/1024.0/1024.0/1024.0, $2/1024.0/1024.0/1024.0) }'
-    EOT
-    interval     = 10
-    timeout      = 1
-  }
 }
 
 module "code-server" {
@@ -121,12 +76,13 @@ module "code-server" {
 }
 
 resource "docker_container" "workspace" {
-  count = data.coder_workspace.me.start_count
   name = "coder-${data.coder_workspace_owner.me.name}-${lower(data.coder_workspace.me.name)}"
   image = "codercom/enterprise-base:ubuntu"
-  hostname = data.coder_workspace.me.name
   entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
-  env     = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
+  env     = [
+    "CODER_AGENT_TOKEN=${coder_agent.main.token}",
+    "DOCKER_HOST=${docker_container.dind.name}:2375"
+  ]
   # runtime = "sysbox-runc"
   privileged = true
 
@@ -135,17 +91,21 @@ resource "docker_container" "workspace" {
     ip   = "host-gateway"
   }
 
-  volumes {
-    container_path = "/home/coder"
-    volume_name    = docker_volume.home_volume.name
-    read_only      = false
+  networks_advanced {
+    name = docker_network.private_network.name
   }
 }
 
-resource "docker_volume" "home_volume" {
-  name = "coder-${data.coder_workspace.me.id}-home"
+resource "docker_network" "private_network" {
+  name = "network-${data.coder_workspace.me.id}"
+}
 
-  lifecycle {
-    ignore_changes = all
+resource "docker_container" "dind" {
+  image      = "docker:dind"
+  privileged = true
+  name       = "dind-${data.coder_workspace.me.id}"
+  entrypoint = ["dockerd", "-H", "tcp://0.0.0.0:2375"]
+  networks_advanced {
+    name = docker_network.private_network.name
   }
 }
