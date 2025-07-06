8bca69bd7223af1ee30d138b73b347ea4947673d
nua <enuesaa@gmail.com>
2025-07-07 01:44:01 +0900

    feat: 一応 gh コマンドを使えるようにした

---
+++ devcontainer-multi-try/.devcontainer/init.sh
@@ -0,0 +1,4 @@
+# #!/bin/bash
+
+# たぶんトークン系は事前に準備する方式の方がいい。
+# GH_TOKEN=$(gh auth token)
--- devcontainer-multi-try/.devcontainer/poststart.sh
+++ devcontainer-multi-try/.devcontainer/poststart.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-pwd
-
 # GO_APP_BRANCH="${GO_APP_BRANCH:-main}"
 # NODE_APP_BRANCH="${NODE_APP_BRANCH:-main}"
 
 # git clone --branch "$GO_APP_BRANCH" https://github.com/enuesaa/loadii.git
 # git clone --branch "$NODE_APP_BRANCH" https://github.com/enuesaa/my-svelte-template.git
+
+gh repo clone my-svelte-template
+
+cd my-svelte-template
+branch=$(gh pr list --assignee @me --state open --limit 1 --json headRefName --jq '.[0].headRefName')
+
+if [ -z "$branch" ] || [ "$branch" = "null" ]; then
+  echo "⚠️ アサインされたPRが見つかりません"
+else
+  echo "🔀 '$branch' に移動します..."
+  gh pr checkout "$branch"
+fi
 
 echo "✅ Setup OK"
