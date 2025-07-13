f83454c5ce98f87a6156bd7a89de148f8b4d7d06
nua <enuesaa@gmail.com>
2025-07-13 16:59:42 +0900

    chore: add sample script create by me

--- app/main.py
+++ app/main.py
@@ -2,3 +2,33 @@ import streamlit as st
 from app.a import aa
 
 st.title(aa())
+st.write("Hello")
+st.error('this is error')
+
+# サイドバー
+with st.sidebar:
+    st.write("hello")
+
+names = st.multiselect(
+    label='select one or two..',
+    options=['a', 'b', 'c'],
+    default=['a', 'b'],
+    help='this is help text',
+)
+
+# 値を変更するたびに stdout される
+# react みたいに再度 render しているのかな
+print(names)
+
+st.markdown(
+    body='''# Hello
+# hello
+```js
+const a = 'b';
+```
+
+''',
+)
+
+# snowflake のロゴが降ってくる
+st.snow()
