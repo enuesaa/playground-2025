1318d27ba9c1d06c59d3b731070ab5fd2797b884
nua <enuesaa@gmail.com>
2025-08-02 16:02:46 +0900

    chore: heroku へデプロイしてみた

---
+++ heroku-go-try/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/", func(c *gin.Context) {
+		c.Data(200, "application/json", []byte(`{"ok": true}`))
+	})
+	router.Run(":" + port)
+}
