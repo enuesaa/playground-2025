aadea2db70cd0be15b0021d6fcfeec9e537e1726
nua <enuesaa@gmail.com>
2025-09-14 17:47:44 +0900

    feat: inject usecase layer

--- app/Http/Components/Counter.php
+++ app/Http/Components/Counter.php
@@ -1,19 +1,29 @@
 <?php
  
 namespace App\Http\Components;
- 
+
+use App\Models\Picture;
+use App\Services\Nasa\Nasa;
+use App\UseCases\AstronomyPictureUseCase;
 use Livewire\Component;
  
 class Counter extends Component
 {
     public $count = 1;
- 
-    public function increment()
+    public Picture $picture;
+
+    public function mount(Nasa $nasa): void
+    {
+        $usecase = new AstronomyPictureUseCase($nasa);
+        $this->picture = $usecase->getNow();
+    }
+
+    public function increment(): void
     {
         $this->count++;
     }
  
-    public function decrement()
+    public function decrement(): void
     {
         $this->count--;
     }
--- resources/views/components/counter.blade.php
+++ resources/views/components/counter.blade.php
@@ -1,7 +1,7 @@
 <div>
     <h1>{{ $count }}</h1>
+    <img src="{{ $picture->image_url }}" alt="{{ $picture->title }}">
  
     <button wire:click="increment">+</button>
- 
     <button wire:click="decrement">-</button>
 </div>
