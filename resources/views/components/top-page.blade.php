f9172b81eda33b38375141a5c9dc56ad3f4e0408
nua <enuesaa@gmail.com>
2025-09-15 19:34:48 +0900

    chore: livewire 由来の UI コンポーネントライブラリ flux を使ってみているが、npm i tailwind をせざるを得ず、伝統的な書き方では無理があるようだ

--- resources/views/components/top-page.blade.php
+++ resources/views/components/top-page.blade.php
@@ -1,7 +1,13 @@
 <div>
+    <flux:navbar>
+        <flux:navbar.item href="/" current>Top</flux:navbar.item>
+        <flux:navbar.item href="/a">a</flux:navbar.item>
+        <flux:navbar.item href="/b">b</flux:navbar.item>
+        <flux:navbar.item href="/c">c</flux:navbar.item>
+    </flux:navbar>
+
     <div>
-        <h1>{{ $count }}</h1>
-    
+        <h1>{{ $count }}</h1>    
         <button wire:click="increment">+</button>
         <button wire:click="decrement">-</button>
     </div>
