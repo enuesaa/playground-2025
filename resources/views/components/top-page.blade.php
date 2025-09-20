07bbded395ea3e8682c8d11e139531093cacfec1
nua <enuesaa@gmail.com>
2025-09-20 16:00:17 +0900

    chore: emove livewire/flux

--- resources/views/components/top-page.blade.php
+++ resources/views/components/top-page.blade.php
@@ -1,11 +1,4 @@
 <div>
-    <flux:navbar>
-        <flux:navbar.item href="/" current>Top</flux:navbar.item>
-        <flux:navbar.item href="/a">a</flux:navbar.item>
-        <flux:navbar.item href="/b">b</flux:navbar.item>
-        <flux:navbar.item href="/c">c</flux:navbar.item>
-    </flux:navbar>
-
     <div>
         <h1>{{ $count }}</h1>    
         <button wire:click="increment">+</button>
