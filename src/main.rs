8602a30feaf58981a4165ed3f43fc5d1ba16618d
nua <enuesaa@gmail.com>
2025-09-23 19:15:13 +0900

    feat: find cakes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod models;
 
 use anyhow::Result;
+use sea_orm::QueryOrder;
 use sea_orm::{ActiveValue::Set, Database, DatabaseConnection};
 use sea_orm::entity::prelude::*;
 use migration::{Migrator, MigratorTrait};
@@ -11,6 +12,8 @@ async fn main() {
         let ret = migrate(&db).await;
         print!("{:?}\n", ret);
         let ret = create_a_cake(&db).await;
+        print!("{:?}\n", ret);
+        let ret = find_cakes(&db).await;
         print!("{:?}\n", ret);
     }
 }
@@ -31,7 +34,16 @@ async fn create_a_cake(db: &DatabaseConnection) -> Result<()> {
         ..Default::default()
     };
     let res = models::cakes::Entity::insert(cake).exec(db).await?;
-    print!("aaa{:?}\n", res);
+    print!("id{:?}\n", res);
+    Ok(())
+}
+
+async fn find_cakes(db: &DatabaseConnection) -> Result<()> {
+    let cakes: Vec<models::cakes::Model> = models::cakes::Entity::find()
+        .order_by_asc(models::cakes::Column::Id)
+        .all(db)
+        .await?;
+    print!("cakes{:?}\n", cakes);
 
     Ok(())
 }
