878d0f745b8381d9d3d0ac7a9323ef7ac2953c58
nua <enuesaa@gmail.com>
2025-09-29 17:16:51 +0900

    chore: write doc for migration because i miss the command

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20250923_094410_create_cakes_table;
+mod m20250929_081344_create_executions_table;
 
 pub struct Migrator;
 
@@ -9,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20250923_094410_create_cakes_table::Migration),
+            Box::new(m20250929_081344_create_executions_table::Migration),
         ]
     }
 }
