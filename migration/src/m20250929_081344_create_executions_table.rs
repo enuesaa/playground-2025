8181955f738c3cad1100e26305a9015b7477591e
nua <enuesaa@gmail.com>
2025-09-29 17:25:38 +0900

    feat: create executions table

--- migration/src/m20250929_081344_create_executions_table.rs
+++ migration/src/m20250929_081344_create_executions_table.rs
@@ -1,41 +1,45 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+#[derive(DeriveIden)]
+enum Executions {
+    Table,
+    Id,
+    Command,
+    Stdout,
+    Stderr,
+    Code,
+    Duration,
+    CreatedAt,
+    UpdatedAt,
+}
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Executions::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(pk_auto(Executions::Id))
+                    .col(string(Executions::Command).not_null())
+                    .col(string(Executions::Stdout))
+                    .col(string(Executions::Stderr))
+                    .col(string(Executions::Code))
+                    .col(integer(Executions::Duration))
+                    .col(timestamp_with_time_zone(Executions::CreatedAt).not_null())
+                    .col(timestamp_with_time_zone(Executions::UpdatedAt).not_null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(Executions::Table).to_owned())
             .await
     }
-}
-
-#[derive(DeriveIden)]
-enum Post {
-    Table,
-    Id,
-    Title,
-    Text,
 }
