aeb74c333f485ee158a0f2f0a2949f226c33d673
nua <enuesaa@gmail.com>
2025-10-06 23:51:03 +0900

    feat: try sqlx with mysql

---
+++ rust-sqlx-mysql-try/src/main.rs
@@ -0,0 +1,100 @@
+use sqlx::{mysql::MySqlPoolOptions, MySqlPool};
+use chrono::NaiveDateTime;
+use anyhow::Result;
+
+struct Note {
+    id: i64,
+    description: String,
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
+}
+impl Default for Note {
+    fn default() -> Self {
+        let now = chrono::Utc::now().naive_utc();
+        Self {
+            id: 0,
+            description: "".into(),
+            created_at: now,
+            updated_at: now,
+        }
+    }
+}
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    database_url: String,
+}
+
+fn loadenv() -> Result<Config> {
+    dotenv::dotenv()?;
+    let config = envy::from_env::<Config>()?;
+    Ok(config)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let config = loadenv()?;
+
+    let dbpool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&config.database_url).await?;
+
+    let mut note = Note{
+        description: "aaa".to_string(),
+        ..Default::default()
+    };
+    create_note(&dbpool, &mut note).await?;
+
+    note.description = "bbb".to_string();
+    update_note(&dbpool, &mut note).await?;
+
+    let notes = list_notes(&dbpool).await?;
+
+    for note in notes {
+        print!("id: {}\n", note.id);
+        print!("description: {}\n", note.description);
+        print!("created_at: {}\n", note.created_at);
+        print!("updated_at: {}\n", note.updated_at);
+        print!("---\n");
+    }
+
+    Ok(())
+}
+
+async fn create_note(dbpool: &MySqlPool, note: &mut Note) -> Result<()> {
+    let id = sqlx::query!(
+        "INSERT INTO notes ( description ) VALUES ( ? )",
+        note.description
+    )
+    .execute(dbpool)
+    .await?
+    .last_insert_id();
+    note.id = id as i64;
+
+    Ok(())
+}
+
+async fn update_note(dbpool:  &MySqlPool, note: &mut Note) -> Result<()> {
+    sqlx::query!(
+        "UPDATE notes SET description = ? WHERE id = ?",
+        note.description,
+        note.id,
+    )
+    .execute(dbpool)
+    .await?;
+
+    Ok(())
+}
+
+async fn list_notes(dbpool:  &MySqlPool) -> Result<Vec<Note>> {
+    let rows = sqlx::query_as!(
+        Note,
+        "SELECT id, description, created_at, updated_at FROM notes ORDER BY id"
+    )
+    .fetch_all(dbpool)
+    .await?;
+
+    Ok(rows)
+}
