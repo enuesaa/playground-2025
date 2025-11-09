1ca6b1a91e6fb10da62e6a93302859115cd95e24
nua <enuesaa@gmail.com>
2025-11-09 16:38:03 +0900

    feat: list memos

--- .devdev/laravel-livewire-v4-memos-app/resources/views/pages/top.blade.php
+++ .devdev/laravel-livewire-v4-memos-app/resources/views/pages/top.blade.php
@@ -1,16 +1,25 @@
 <?php
 
+use App\Models\Memo;
 use Livewire\Component;
+use Livewire\Attributes\Computed;
 
 new class extends Component
-{
-    //
+{ 
+    #[Computed]
+    public function memos()
+    {
+        return Memo::all();
+    }
 };
 ?>
 
 <div>
     <h1 class="text-3xl font-bold underline">top page</h1>
-    top
+
+    @foreach ($this->memos as $memo)
+        <article>{{ $memo->title }}</article>
+    @endforeach
 
     <a href="/post/create">create</a>
 </div>
