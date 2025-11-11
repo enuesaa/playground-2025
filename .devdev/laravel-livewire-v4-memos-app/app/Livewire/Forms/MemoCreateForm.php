449304d30f2f55f07862db763bca227015151541
nua <enuesaa@gmail.com>
2025-11-11 23:10:50 +0900

    chore: require pint and format

--- .devdev/laravel-livewire-v4-memos-app/app/Livewire/Forms/MemoCreateForm.php
+++ .devdev/laravel-livewire-v4-memos-app/app/Livewire/Forms/MemoCreateForm.php
@@ -9,7 +9,7 @@ class MemoCreateForm extends Form
 {
     #[Validate('required|max:100', message: 'Please provide a title')]
     public $title = '';
- 
+
     #[Validate('required')]
     public $content = '';
 }
