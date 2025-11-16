9a8b3012de7f3545cbb0753605f61b9ba2a2d10d
nua <enuesaa@gmail.com>
2025-11-16 19:41:51 +0900

    chore: update

--- amplitude-try/sampleapp/src/pages/index.tsx
+++ amplitude-try/sampleapp/src/pages/index.tsx
@@ -6,6 +6,9 @@ export default function Page() {
       <div>aaaa</div>
       <div>ccc</div>
       <span>aa</span>
+
+      <img src="https://images.ctfassets.net/8aevphvgewt8/5fErhOtgvjrf97d7wOoARB/b262e06c615977f33046c468147aa114/screenshot-windows-dark.png?w=2496&fm=webp&q=90" width="500px" />
+      bb
     </>
   )
 }
