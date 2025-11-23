c93c474c9584c97760ef78fd661bdd9d963bc465
nua <enuesaa@gmail.com>
2025-11-23 15:03:51 +0900

    chore: 一瞬スタイルが読み込めない問題を解決した

--- .devdev/laravel-livewire-v4-memos-app/resources/views/layouts/app.blade.php
+++ .devdev/laravel-livewire-v4-memos-app/resources/views/layouts/app.blade.php
@@ -5,13 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <!-- styles -->
-        <script src="https://cdn.jsdelivr.net/npm/@unocss/runtime"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@unocss/runtime/uno.global.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@unocss/reset/tailwind-compat.min.css" />
-
+        <style>
+            /* https://unocss.dev/integrations/runtime#preventing-fouc */
+            [un-cloak] {
+                display: none;
+            }
+        </style>
         <title>{{ $title ?? 'Page Title' }}</title>
     </head>
 
-    <body class="min-h-screen bg-slate-50 text-slate-900 antialiased">
+    <body class="min-h-screen bg-slate-50 text-slate-900 antialiased" un-cloak>
         <livewire:header />
 
         <main class="max-w-4xl mx-auto p-4 min-h-dvh">
