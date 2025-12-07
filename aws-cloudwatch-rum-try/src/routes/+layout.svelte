31b1b7d100e329a2035d087e134d8ad046b8cd83
nua <enuesaa@gmail.com>
2025-12-07 16:48:05 +0900

    feat: trying aws cloudwatch rum with svelte

---
+++ aws-cloudwatch-rum-try/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import './app.css'
+	import Header from './Header.svelte'
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
+	import CloudWatchRumProvider from './CloudWatchRumProvider.svelte'
+
+	const queryClient = new QueryClient()
+</script>
+
+<svelte:head>
+	<title>my-svelte-template</title>
+</svelte:head>
+
+<CloudWatchRumProvider />
+<QueryClientProvider client={queryClient}>
+	<Header />
+	<main class="container mx-auto">
+		<slot />
+	</main>
+</QueryClientProvider>
---
+++ aws-cloudwatch-rum-try/src/routes/CloudWatchRumProvider.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { AwsRum } from 'aws-rum-web';
+	import type { AwsRumConfig } from 'aws-rum-web';
+	import { PUBLIC_CWRUM_APPLICATION_ID } from '$env/static/public'
+	import { onMount } from 'svelte'
+
+	$effect(() => {
+		try {
+			const config: AwsRumConfig = {
+				sessionSampleRate: 1 ,
+				endpoint: "https://dataplane.rum.ap-northeast-1.amazonaws.com" ,
+				telemetries: ["performance","errors","http"] ,
+				allowCookies: true,
+				enableXRay: false,
+				signing: true,
+				enableRumClient: true,
+				dispatchInterval: 1000,
+			};
+			const awsRum = new AwsRum(
+				PUBLIC_CWRUM_APPLICATION_ID,
+				'1.0.0',
+				'ap-northeast-1',
+				config
+			);
+			awsRum.dispatch()
+
+			console.log('a', awsRum)
+
+			awsRum.recordPageView('aaa');
+		} catch (error) {
+			console.error(error)
+		}
+	})
+</script>
