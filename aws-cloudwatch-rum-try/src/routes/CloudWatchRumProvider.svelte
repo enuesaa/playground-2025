ad25af610b45e0a0214d608148bc5f2588f19403
nua <enuesaa@gmail.com>
2025-12-10 21:06:27 +0900

    chore: 不要なコードを削除

--- aws-cloudwatch-rum-try/src/routes/CloudWatchRumProvider.svelte
+++ aws-cloudwatch-rum-try/src/routes/CloudWatchRumProvider.svelte
@@ -1,28 +1,20 @@
 <script lang="ts">
-	import { AwsRum } from 'aws-rum-web';
-	import type { AwsRumConfig } from 'aws-rum-web';
+	import { AwsRum } from 'aws-rum-web'
+	import type { AwsRumConfig } from 'aws-rum-web'
 	import { PUBLIC_CWRUM_APPLICATION_ID } from '$env/static/public'
 	import { onMount } from 'svelte'
 
 	onMount(() => {
 		try {
 			const config: AwsRumConfig = {
-				sessionSampleRate: 1 ,
-				endpoint: "https://dataplane.rum.ap-northeast-1.amazonaws.com" ,
-				telemetries: ["performance","errors","http"] ,
+				sessionSampleRate: 1,
+				endpoint: 'https://dataplane.rum.ap-northeast-1.amazonaws.com',
+				telemetries: ['performance', 'errors', 'http'],
 				allowCookies: true,
 				enableXRay: false,
-				signing: false
-			};
-			const awsRum = new AwsRum(
-				PUBLIC_CWRUM_APPLICATION_ID,
-				'1.0.0',
-				'ap-northeast-1',
-				config
-			);
-			// awsRum.dispatch()
-			// console.log('a', awsRum)
-			// awsRum.recordPageView('aaa');
+				signing: false,
+			}
+			const awsRum = new AwsRum(PUBLIC_CWRUM_APPLICATION_ID, '1.0.0', 'ap-northeast-1', config)
 		} catch (error) {
 			console.error(error)
 		}
