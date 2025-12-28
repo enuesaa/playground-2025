af9ab1e6fe056ac5878285d55e1b7c431580a677
nua <enuesaa@gmail.com>
2025-12-28 17:00:23 +0900

    feat: llm module から cores3 へURLを送り、そのレスポンスを受け取る

--- m5stack-llmmodule/receive/README.md
+++ m5stack-llmmodule/receive/README.md
@@ -1,10 +1,10 @@
-### llm module から cores3 へテキストを送る
+### llm module から cores3 へURLを送り、そのレスポンスを受け取る
 
-cores3 は記載のコード。
-llm module では下記を実行
+llm module で下記を実行
 
 ```bash
 systemctl stop llm-sys
 stty -F /dev/ttyS1 921600 raw -echo -echoe -echok
-echo "hello from LLM" > /dev/ttyS1
+echo "https://example.com/" > /dev/ttyS1
+cat /dev/ttyS1
 ```
--- m5stack-llmmodule/receive/src/main.cpp
+++ m5stack-llmmodule/receive/src/main.cpp
@@ -1,20 +1,34 @@
-#include <Arduino.h>
 #include <M5Unified.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 void setup() {
-  // see https://qiita.com/nnn112358/items/5c2dd73226ba21f6c02a
   M5.begin();
   M5.Display.setTextSize(2);
+
   M5.Display.println("start");
+  WiFi.begin("", "");
+  while (WiFi.status() != WL_CONNECTED) delay(100);
+
+  M5.Display.println("conected");
 
   Serial2.begin(921600, SERIAL_8N1, 18, 17);
+  M5.Display.println("ready");
 }
 
 void loop() {
-  if (Serial2.available()) {
-    String data = Serial2.readStringUntil('\n');
-    if (data.length() > 0) {
-      M5.Display.println("receive: " + data);
-    }
-  }
+  if (!Serial2.available()) return;
+
+  String url = Serial2.readStringUntil('\n');
+  url.trim();
+  if (url.length() == 0) return;
+
+  HTTPClient http;
+  http.begin(url);
+  int code = http.GET();
+  String body = http.getString();
+  http.end();
+
+  Serial2.println(body);
+  M5.Display.println("ok");
 }
